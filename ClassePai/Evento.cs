821ca4726744ad823c34a0596090054d73783531
Alander Machado <alandermm@hotmail.com>
2017-11-10 20:38:07 -0200

    Atualização

--- ClassePai/Evento.cs
+++ ClassePai/Evento.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+
 namespace ProjetoEvento.ClassePai
 {
     public abstract class Evento
@@ -9,5 +11,19 @@ namespace ProjetoEvento.ClassePai
         public string Duracao {get; set;}
         public DateTime Data {get; set;}
         public int Classificacao {get; set;}
+
+        public virtual bool Cadastrar(){
+            return false;
+        }
+
+        public virtual List<string> Pesquisar(DateTime DataEvento){
+            return null;
+        }
+
+        public virtual string Pesquisar(string TituloEvento){
+            return null;
+        }
+
+        
     }
 }
